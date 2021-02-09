47be449025fa54fe0909588e2a958dee3577f71f
Sohanur Rahman <sohanur064@gmail.com>
2021-02-09 17:08:42 +0600

    Dianamic navigation with categories

--- includes/db.php
+++ includes/db.php
@@ -12,9 +12,9 @@ foreach($db as $key => $value){
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
 // Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}else{
-    echo "successfully connected!";
-}
+// if (mysqli_connect_errno()) {
+//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//   exit();
+// }else{
+//     echo "successfully connected!";
+// }
